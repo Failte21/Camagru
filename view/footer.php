7e4625be057cc0b28f718f8cff46c93386630451
failte21 <failte@openmailbox.org>
2017-05-06 15:18:21 +0200

    footer arranged

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,9 @@
 <footer>
-  <a href="#">Just want to explore the gallery ?<br>
-    Click here !</a>
-</footer>
+  <?php
+    if (!isset($_SESSION['logged_user'])){
+      ?>
+      <a href="#">Just want to explore the gallery ?<br>
+        Click here !</a>
+      <?php
+    }
+  ?></footer>
