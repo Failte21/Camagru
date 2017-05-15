462cc2466c47470fb49c6ea76450e8ea82a3d156
Benoit CORBEL <becorbel@e2r12p12.42.fr>
2017-05-15 16:28:49 +0200

    ajout

--- check/delete.php
+++ check/delete.php
@@ -5,5 +5,9 @@
   if ($db = initDb()){
     $query = $db->prepare('DELETE FROM picture WHERE id = ?');
     $query->execute(array(htmlentities($_GET['id'])));
+    $query = $db->prepare('DELETE FROM like_picture WHERE id_picture LIKE ?');
+    $query->execute(array($_GET['id']));
+    $query = $db->prepare('DELETE FROM comment_picture WHERE id_picture LIKE ?');
+    $query->execute(array($_GET['id']));
   }
 ?>
