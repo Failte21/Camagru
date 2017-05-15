9d8b53e95ecc2d76464ba0c8070b803018681998
Benoit CORBEL <becorbel@e2r12p12.42.fr>
2017-05-15 14:17:40 +0200

    ajout

--- view/header.php
+++ view/header.php
@@ -4,12 +4,12 @@
     if (isset($_SESSION['logged_user'])){
       ?>
       <ul>
-        <li><a href="/camagru/index.php?page=gallery&pageNb=0">Explorer</a></li>
-        <li><a href="/camagru/index.php?page=my_gallery&pageNb=0">My gallery</a></li>
+        <li><a href="/camagru2/index.php?page=gallery&pageNb=0">Explorer</a></li>
+        <li><a href="/camagru2/index.php?page=my_gallery&pageNb=0">My gallery</a></li>
         <li><a><?php echo $_SESSION['logged_user'] ?></a>
           <ul>
             <li><a href="index.php?page=settings">Settings</a></li>
-            <li><a href="/camagru/check/log_out.php">Log out</a></li>
+            <li><a href="/camagru2/check/log_out.php">Log out</a></li>
           </ul>
         </li>
       </ul>
